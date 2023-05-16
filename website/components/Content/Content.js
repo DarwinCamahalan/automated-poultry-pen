9731164f190969863a97b47710833d7d69a7ca95
DarwinCamahalan <camahalandarwin@gmail.com>
2023-05-16 22:25:51 +0800

    Enhance UI 1.0.2

--- website/components/Content/Content.js
+++ website/components/Content/Content.js
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
 import styles from "./content.module.scss";
-import { db, storage } from "../firebaseConfig";
+import { db } from "../firebaseConfig";
 import { onValue, ref } from "firebase/database";
 import {
   getStorage,
   ref as storageRef,
   getDownloadURL,
 } from "firebase/storage";
+
+import { motion } from "framer-motion";
+
+import { BsCalendar4Week, BsFan, BsDeviceSsd } from "react-icons/bs";
+import { RiSensorLine } from "react-icons/ri";
+import { FaTemperatureLow } from "react-icons/fa";
+import { BiCameraHome } from "react-icons/bi";
+import { GoLightBulb } from "react-icons/go";
 
 const Content = () => {
   const [temperature, setTemperature] = useState(null);
@@ -31,7 +39,7 @@ const Content = () => {
 
     // Fetch the image URL from Firebase Storage
     const storageInstance = getStorage();
-    const imageRef = storageRef(storageInstance, "image_capture.png");
+    const imageRef = storageRef(storageInstance, "image_capture.jpg");
 
     const fetchImageUrl = () => {
       getDownloadURL(imageRef)
@@ -46,8 +54,8 @@ const Content = () => {
     // Fetch the initial image URL
     fetchImageUrl();
 
-    // Fetch a new image URL every 500 millisecond
-    const interval = setInterval(fetchImageUrl, 100);
+    // Fetch a new image URL every 1 second
+    const interval = setInterval(fetchImageUrl, 1000);
 
     // Clean up the interval when the component is unmounted
     return () => clearInterval(interval);
@@ -55,32 +63,71 @@ const Content = () => {
 
   return (
     <div className={styles.contentBG}>
+      <div className={styles.info}>
+        <div className={styles.dayCount}>
+          <BsCalendar4Week className={styles.dayIcon} />
+          <h1>Day</h1>
+          <p>
+            <span>1</span>
+          </p>
+        </div>
+
+        <div className={styles.fan}>
+          <BsFan className={styles.fanIcon} />
+          <h1>Fan</h1>
+          <p>
+            Status: <span>OFF</span>
+          </p>
+        </div>
+
+        <div className={styles.motor}>
+          <RiSensorLine className={styles.motorIcon} />
+          <h1>Motor</h1>
+          <p>
+            Status: <span>OFF</span>
+          </p>
+        </div>
+
+        <div className={styles.bulb}>
+          <GoLightBulb className={styles.bulbIcon} />
+          <h1>Bulb</h1>
+          <p>
+            Status: <span>OFF</span>
+          </p>
+        </div>
+      </div>
       <div className={styles.cameraImage}>
         {imageUrl && <img src={imageUrl} alt="Camera" />}
       </div>
       <div className={styles.sensors}>
-        <div className={styles.dayCount}>
-          <h1>Day</h1>
-          <p>1</p>
+        <div className={styles.averageTemp}>
+          <FaTemperatureLow className={styles.avgTempIcon} />
+          <h1>Average Temp.</h1>
+          <p>
+            <span>{temperature + cameraTemperature / 2} °C</span>
+          </p>
         </div>
         <div className={styles.dhtSensor}>
+          <BsDeviceSsd className={styles.dhtIcon} />
           <h1>DHT11</h1>
-          <p>Temperature: {temperature} °C</p>
-          <p>Humidity: {humidity} %</p>
+          <p>
+            Temperature: <span>{temperature} °C</span>
+          </p>
+          <p>
+            Humidity: <span>{humidity} %</span>
+          </p>
         </div>
         <div className={styles.camSensor}>
+          <BiCameraHome className={styles.camIcon} />
           <h1>MLX90640</h1>
-          <p>Temperature: {cameraTemperature} °C</p>
+          <p>
+            Temperature: <span>{cameraTemperature} °C</span>
+          </p>
         </div>
 
-        <div className={styles.fan}>
-          <h1>Fan</h1>
-          <p>Status: OFF</p>
-        </div>
-
-        <div className={styles.motor}>
-          <h1>Motor</h1>
-          <p>Status: OFF</p>
+        <div className={styles.date}>
+          <h1>Start Date</h1>
+          <p>5/16/2023</p>
         </div>
       </div>
     </div>
