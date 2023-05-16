8956401f8d31899563179461489f6edd3ebda9b4
DarwinCamahalan <camahalandarwin@gmail.com>
2023-05-16 18:35:45 +0800

    Enhance UI

--- website/components/Content/Content.js
+++ website/components/Content/Content.js
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
 import styles from "./content.module.scss";
-import { db } from "../firebaseConfig";
+import { db, storage } from "../firebaseConfig";
 import { onValue, ref } from "firebase/database";
+import {
+  getStorage,
+  ref as storageRef,
+  getDownloadURL,
+} from "firebase/storage";
 
 const Content = () => {
   const [temperature, setTemperature] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [cameraTemperature, setCameraTemperature] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
 
   useEffect(() => {
     const dhtSensorRef = ref(db, "dht_sensor");
@@ -22,19 +28,60 @@ const Content = () => {
       const temperature = snapshot.val().temperature;
       setCameraTemperature(Math.round(temperature));
     });
+
+    // Fetch the image URL from Firebase Storage
+    const storageInstance = getStorage();
+    const imageRef = storageRef(storageInstance, "image_capture.png");
+
+    const fetchImageUrl = () => {
+      getDownloadURL(imageRef)
+        .then((url) => {
+          setImageUrl(url);
+        })
+        .catch((error) => {
+          console.log("Error fetching image URL:", error);
+        });
+    };
+
+    // Fetch the initial image URL
+    fetchImageUrl();
+
+    // Fetch a new image URL every 500 millisecond
+    const interval = setInterval(fetchImageUrl, 100);
+
+    // Clean up the interval when the component is unmounted
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className={styles.contentBG}>
-      <div className={styles.dhtSensor}>
-        <h1>DHT11</h1>
-        <p>Temperature: {temperature} °C</p>
-        <p>Humidity: {humidity} %</p>
+      <div className={styles.cameraImage}>
+        {imageUrl && <img src={imageUrl} alt="Camera" />}
       </div>
+      <div className={styles.sensors}>
+        <div className={styles.dayCount}>
+          <h1>Day</h1>
+          <p>1</p>
+        </div>
+        <div className={styles.dhtSensor}>
+          <h1>DHT11</h1>
+          <p>Temperature: {temperature} °C</p>
+          <p>Humidity: {humidity} %</p>
+        </div>
+        <div className={styles.camSensor}>
+          <h1>MLX90640</h1>
+          <p>Temperature: {cameraTemperature} °C</p>
+        </div>
 
-      <div className={styles.camSensor}>
-        <h1>MLX90640 Camera</h1>
-        <p>Temperature: {cameraTemperature} °C</p>
+        <div className={styles.fan}>
+          <h1>Fan</h1>
+          <p>Status: OFF</p>
+        </div>
+
+        <div className={styles.motor}>
+          <h1>Motor</h1>
+          <p>Status: OFF</p>
+        </div>
       </div>
     </div>
   );
--- website/components/firebaseConfig.js
+++ website/components/firebaseConfig.js
@@ -1,18 +1,21 @@
-import { initializeApp } from 'firebase/app'
-import { getDatabase } from 'firebase/database'
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBq68owsBjnpM6KRiFdJm41nd5mSpKBaW0",
-    authDomain: "poultry-monitoring-system-1.firebaseapp.com",
-    databaseURL: "https://poultry-monitoring-system-1-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "poultry-monitoring-system-1",
-    storageBucket: "poultry-monitoring-system-1.appspot.com",
-    messagingSenderId: "265095833387",
-    appId: "1:265095833387:web:43286dabe51d0c325a476e"
-}
+  apiKey: "AIzaSyBq68owsBjnpM6KRiFdJm41nd5mSpKBaW0",
+  authDomain: "poultry-monitoring-system-1.firebaseapp.com",
+  databaseURL:
+    "https://poultry-monitoring-system-1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "poultry-monitoring-system-1",
+  storageBucket: "poultry-monitoring-system-1.appspot.com",
+  messagingSenderId: "265095833387",
+  appId: "1:265095833387:web:43286dabe51d0c325a476e",
+};
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const storage = getStorage(app);
 
-export const db = getDatabase()
+export { app, db, storage };
