1a2dea0ed6892b951729c6173952077bdc69bc55
DarwinCamahalan <camahalandarwin@gmail.com>
2023-05-16 18:33:38 +0800

    Updated the Image View

--- hardware/app.py
+++ hardware/app.py
@@ -8,6 +8,7 @@ import Adafruit_DHT
 import matplotlib.pyplot as plt
 import cv2
 import os
+from scipy import ndimage
 
 # Configure Firebase with your credentials
 config = {
@@ -32,25 +33,29 @@ def read_dht11_sensor():
     return humidity, temperature
 
 # Set up the MLX90640 infrared camera
-i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
-frame = np.zeros((24 * 32,))
 mlx_shape = (24, 32)
 
+mlx_interp_val = 10
+mlx_interp_shape = (mlx_shape[0] * mlx_interp_val, mlx_shape[1] * mlx_interp_val)
 
-# Setup the figure for plotting
-plt.ion() # enables interactive plotting
-fig, ax = plt.subplots(figsize=(12, 7))
-therm1 = ax.imshow(np.zeros(mlx_shape), vmin=0, vmax=60, cmap='jet') # start plot with zeros and jet colormap
-cbar = fig.colorbar(therm1) # setup colorbar for temps
-cbar.set_label('Temperature [$^{\circ}$C]', fontsize=14) # colorbar label
+fig = plt.figure(figsize=(12, 9))
+ax = fig.add_subplot(111)
+fig.subplots_adjust(0.05, 0.05, 0.95, 0.95)
+therm1 = ax.imshow(np.zeros(mlx_interp_shape), interpolation='none', cmap=plt.cm.bwr, vmin=25, vmax=45)
+cbar = fig.colorbar(therm1)
+cbar.set_label('Temperature [$^{\circ}$C]', fontsize=14)
 
-frame = np.zeros((24*32,)) # setup array for storing all 768 temperatures
+fig.canvas.draw()
+ax_background = fig.canvas.copy_from_bbox(ax.bbox)
+fig.show()
+
+frame = np.zeros(mlx_shape[0] * mlx_shape[1])
 t_array = []
 snapshot_filename = "image_capture.png"
 print("Starting loop")
-
 
 while True:
     # Read data from DHT11 sensor
@@ -79,64 +84,33 @@ while True:
     # Print MLX90640 temperature
     print("\nMLX90640 Sensor:")
     print("Average Temperature: {0:2.1f}°C".format(average_temperature))
-    
+
     t1 = time.monotonic()
     try:
-        mlx.getFrame(frame) # read MLX temperatures into frame var
-        data_array = np.reshape(frame, mlx_shape) # reshape to 24x32
+        mlx.getFrame(frame)
+        data_array = np.reshape(frame, mlx_shape)
+        data_array = ndimage.zoom(data_array, mlx_interp_val)
+        therm1.set_array(data_array)
+        therm1.set_clim(vmin=np.min(data_array), vmax=np.max(data_array))
+        cbar.update_normal(therm1)
+        ax.draw_artist(therm1)
+        fig.canvas.blit(ax.bbox)
+        fig.canvas.flush_events()
 
-        # Interpolate the data to a higher resolution
-        interp_data = cv2.resize(data_array, (256, 192), interpolation=cv2.INTER_CUBIC)
-
-        # Update the plot with interpolated data
-        therm1.set_data(np.fliplr(interp_data)) # flip left to right
-        therm1.set_clim(vmin=np.min(interp_data), vmax=np.max(interp_data)) # set bounds
-
-        plt.title(f"Max Temp: {np.max(data_array):.1f}C")
-
-        # Calculate average temperature
-        avg_temp = np.mean(data_array)
-
-        # Highlight hottest and coldest pixels
-        max_temp = np.max(data_array)
-        min_temp = np.min(data_array)
-
-        # Apply temperature color mapping to the plot
-        therm1.set_cmap('jet')
-
-        # Draw text annotations for hottest, coldest, and average temperatures
-        ax.text(0, -2, f"Max: {max_temp:.1f}°C", color='red', fontsize=12)
-        ax.text(16, -2, f"Avg: {avg_temp:.1f}°C", color='yellow', fontsize=12)
-        ax.text(28, -2, f"Min: {min_temp:.1f}°C", color='darkblue', fontsize=12)
-
-        cbar.update_normal(therm1) # update colorbar range
-
-        plt.pause(0.001) # required
         t_array.append(time.monotonic() - t1)
-        print('Sample Rate: {0:2.1f}fps'.format(len(t_array) / np.sum(t_array)))
+        if len(t_array) > 10:
+            t_array = t_array[1:]
+        print('Frame Rate: {0:2.1f}fps'.format(len(t_array) / np.sum(t_array)))
 
-        # # Save snapshot image
-        # snapshot_count += 1
-        # image_filename = f"snapshot_{snapshot_count}.png"
-        # fig.savefig(image_filename, dpi=300)
-        # print(f"Snapshot saved: {image_filename}")
-        
-        # storage.child(image_filename).put(image_filename)
-        # print(f"Snapshot sent to Firebase Storage: {image_filename}")
-        
         # Save snapshot image
-        fig.savefig(snapshot_filename, dpi=300)
-        print(f"Snapshot saved: {snapshot_filename}")
+        fig.savefig(snapshot_filename, dpi=300, facecolor='#FCFCFC', bbox_inches='tight')
 
         # Upload snapshot image to Firebase Storage
         storage.child(snapshot_filename).put(snapshot_filename)
-        print(f"Snapshot sent to Firebase Storage: {snapshot_filename}")
 
         # Delete the local snapshot image after uploading to Firebase Storage
         os.remove(snapshot_filename)
-        
-    except ValueError:
-        continue # if error, just read again
 
-    # Wait for a specified time before reading again
-    time.sleep(1)
+    except ValueError:
+        continue  # if error, just read again
+
