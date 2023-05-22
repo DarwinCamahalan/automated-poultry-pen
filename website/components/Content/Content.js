f0cbc1ab946db3e9642465010b8cbf730b37d210
DarwinCamahalan <camahalandarwin@gmail.com>
2023-05-22 14:13:28 +0800

    Fix Fan Icon Bug

--- website/components/Content/Content.js
+++ website/components/Content/Content.js
@@ -119,7 +119,7 @@ const Content = () => {
   };
 
   const iconStyle = () => {
-    if (bulbStatus === "OFF") return styles.fanIcon;
+    if (fanStatus === "OFF") return styles.fanIcon;
     else return styles.fanON;
   };
 
